362f0c9a3ce14dad7ce3b9e5bb4aa149b7e6739c
Khanyisa Mgunculu <khanyisamgunculu@gmail.com>
2023-03-29 16:44:48 +0200

    js_fundamentals

---
+++ js-fundementals/fundementals.js
@@ -0,0 +1,39 @@
+// var a = 1;
+// var a = 10;
+//let a = 16;
+// console.log(a);
+// let a = 1;
+// let c = a+b;
+// console.log(c);
+// let d = 10
+// let c = 10  
+// let a = 10
+// let b = 20;
+// if (a==b&&c==d) {
+//      console.log ("true");
+//  } else {
+//     console.log ("false");
+//  }
+
+// let a = 10
+// if (a > 10){
+//     console.log("Greater than a decade");
+// } else if (a ==10){
+//     console.log("it is a decade")
+// } else if (a<10 || a >=0){
+//     console.log("it is lesser than a decade")
+// }else {
+//     console.log("we cannot have years!")
+// }
+
+let a = 5;
+let b = 10;
+let c = 30;
+
+if ( a > b && a > c) { 
+    console.log("it is not a maximum")
+} else if ( b > a && b > c ) {
+    console.log("it is a middle number")
+} else if ( c > a && c > b) {
+    console.log("it is a maximum")
+}

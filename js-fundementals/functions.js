07ffb826913c9155deaf80bff82e3f94cb23ddfd
Khanyisa Mgunculu <khanyisamgunculu@gmail.com>
2023-03-30 16:50:54 +0200

    functions.js

---
+++ js-fundementals/functions.js
@@ -0,0 +1,41 @@
+// let a = 20;
+// let b = 3;
+// if ( b % 2 === 0 ) {
+//     console.log("it is not an even number")
+// }
+// else if ( a % 2 === 0 ){
+//  console.log("it is an even number")
+// } 
+
+// let c = 7
+// let d = 9
+// if ( c % 2 === 1 ) {
+//     console.log("it is an evennumber")
+// }
+// else if (a % 2 === 1 ) {
+//     console.log("it is an odd number")
+// }
+
+// function checkEvenNumber() {
+//     var name = document.getElementById("even number").value
+
+//     if ( c % 2 === 1) {
+//         alert(number + " " + "it is an even number")
+//     }
+//         else if ( a & 2 === 1 ) {
+//         alert( number + " " + "it is an odd number")
+         
+//         }
+
+// }
+
+
+// let i = 0
+let numbers =[2,9,11,7,5,3,4,8,15]
+        
+for( let i = 0; i <=9; i ++ ){
+
+    if ( numbers[i] % 2 === 0) {
+    console.log(numbers[i])
+    }
+}
